642f12ca7a965c0179cf432aae5bae79640cbc55
ShineySun <ksp2246@naver.com>
2021-02-07 00:03:25 +0900

    vanishing point

--- LSTM/interpolation_fitting.py
+++ LSTM/interpolation_fitting.py
@@ -15,6 +15,141 @@ import vis_util
 import monospline as mnsp
 import interpolation
 
+def curve_fit_mode(x_data,y_data, mode=1, result_img = None):
+        def objective_linear(x, a, b):
+	           return a * x + b
+
+        def objective_quadratic(x,a,b,c):
+            return a*x + b*x**2 + c
+
+        def objective_cubic(x,a,b,c,d):
+            return a*x + b*x**2 + c*x**3 + d
+
+        def objective_quartic(x,a,b,c,d,e):
+	        return a*x + b*x**2 + c*x**3 + d*x**4 + e
+
+        return_list = []
+
+        for batch in range(len(x_data)):
+            new_x = []
+            new_y = []
+            batch_len = len(x_data[batch])
+            batch_len = 0
+
+            #print("class num : {}".format(len(x_data)))
+
+            for idx in range(len(x_data[batch])):
+                if idx >= batch_len:
+                    new_x.append(x_data[batch][idx])
+                    new_y.append(y_data[batch][idx])
+                    # curve fit
+            if mode == 1:
+                try:
+                    popt, _ = curve_fit(objective_linear,new_x, new_y)
+                    a,b = popt
+                except:
+                    continue
+            elif mode == 2:
+                try:
+                    popt, _ = curve_fit(objective_quadratic, new_x, new_y)
+                    a,b,c = popt
+                except:
+                    continue
+            elif mode == 3:
+                try:
+                    popt, _ = curve_fit(objective_cubic, new_x, new_y)
+                    a,b,c,d = popt
+                except:
+                    continue
+            elif mode == 4:
+                try:
+                    popt, _ = curve_fit(objective_quartic, new_x, new_y)
+                    a,b,c,d,e = popt
+                except:
+                    continue
+            # plot input vs output
+            #pyplot.scatter(new_x, new_y)
+
+            # define a sequence of inputs between the smallest and largest known inputs
+            #x_line = arange(min(new_x), max(new_x), 1)
+            x_line = np.arange(0, 720, 1)
+
+            # calculate the output for the range
+            if mode == 1:
+                y_line = objective_linear(x_line, a, b)
+            elif mode == 2:
+                y_line = objective_quadratic(x_line, a, b, c)
+            elif mode == 3:
+                y_line = objective_cubic(x_line,a, b, c, d)
+            elif mode == 4:
+                y_line = objective_quartic(x_line,a, b, c, d, e)
+
+            tmp_list = []
+
+            if result_img is not None:
+                for x,y in zip(x_line, y_line):
+                    try:
+                        return_list.append([int(y), int(x)])
+                        cv2.circle(result_img, (int(y),int(x)), 1, (0,255,255))
+                    except OverflowError:
+                        print("Overflow")
+                        continue
+        # cv2.imshow("result_img", result_img)
+        # cv2.waitKey(-1)
+        return return_list
+
+def vanish_point(fit_points, result_img):
+        #print(fit_points)
+        numpy_fit_points = np.array(fit_points)
+        #print("len(fit_points) : {}".format(len(fit_points)))
+        #print(numpy_fit_points.shape)
+
+        score_matrix = np.zeros((len(numpy_fit_points),len(numpy_fit_points)))
+
+        for i in range(len(numpy_fit_points)-1):
+            for j in range(i+1, len(numpy_fit_points)):
+                dist = np.linalg.norm(numpy_fit_points[i][:2] - numpy_fit_points[j][:2],2)
+                #print(dist)
+
+                if dist < 1:
+                    dist = 1.0
+
+                score_matrix[i][j] = 1/dist
+                score_matrix[j][i] = score_matrix[i][j]
+
+        sum_score_matrix = score_matrix.sum(axis=1)
+        sum_score_vector = sum_score_matrix.sum()
+
+        score_vector = sum_score_matrix/sum_score_vector
+        sorted_score_vector = np.sort(score_vector)[::-1]
+
+        score_idx = []
+
+        for i in range(len(sorted_score_vector)):
+            tmp_index = np.where(score_vector == sorted_score_vector[i])
+
+            try:
+                score_idx.append(tmp_index[0][0])
+            except:
+                return None
+
+        num_point_rank = min(10, len(fit_points))
+
+        topN_vp = []
+
+        for i in range(num_point_rank):
+            topN_vp.append(fit_points[score_idx[i]][:2])
+
+        mean_topN_vp_x = np.mean(topN_vp[:], axis=0)
+        cv2.circle(result_img, (int(mean_topN_vp_x[0]),int(mean_topN_vp_x[1])),6,(255,0,255),3)
+
+        # cv2.imshow('result_img', result_img)
+        # cv2.waitKey(-1)
+
+        return mean_topN_vp_x
+
+
+
 def main():
     # Select GPU
     os.environ["CUDA_VISIBLE_DEVICES"] = '0'
@@ -33,7 +168,7 @@ def main():
     two_train_data, two_test_data = dataset.get_instance(option = 0)
 
     for idx_, train_data in enumerate(two_train_data):
-        if idx_ == 6: break
+        if idx_ == 20: break
         # image clip directory
         clip_dir = dataset.train_path + train_data['raw_file']
         print("* clip dir : {}".format(clip_dir))
@@ -76,8 +211,7 @@ def main():
         quadratic_intrp_image = vis_util.draw_points(quadratic_intrp_image, quadratic_intrp_sets)
 
         #----------------------------------------------------------------------------------------------------------------#
-
-        seq_len_ = 30
+        seq_len_ = 2
 
         train_sets, test_sets = dataset.split_train_test(mono_spline_sets, test_size = 3, seq_len = seq_len_)
 
@@ -175,6 +309,8 @@ def main():
 
         test_image = raw_image.copy()
 
+        vp_points = []
+
         with torch.no_grad():
             color_ = [(255,100,0), (100,255,0), (100,120,255), (255,100,100), (0,120,80)]
 
@@ -200,13 +336,18 @@ def main():
 
                 for pt in predict_pair:
                     cv2.circle(annot_image, (int(pt[0]), int(pt[1])), radius=2, color = color_[idx], thickness=2)
+                    vp_points.append(pt)
+
+        real_vp_1 = vanish_point(vp_points, annot_image)
                 # for pt in gt_pair:
                 #     cv2.circle(test_image, (int(pt[0]), int(pt[1])), radius=2, color = color_[idx+1], thickness=2)
 
 
 
 
-        #cv2.imshow("raw_image", raw_image)
+
+
+        # cv2.imshow("raw_image", annot_image)
         '''
         cv2.imwrite("/home/sun/Desktop/Vanishing-Point-Detection/interpolation_image/annot_image.jpg", annot_image)
         cv2.imwrite("/home/sun/Desktop/Vanishing-Point-Detection/interpolation_image/cubic_spline_image.jpg", cubic_spline_image)
@@ -215,8 +356,8 @@ def main():
         cv2.imwrite("/home/sun/Desktop/Vanishing-Point-Detection/interpolation_image/quadratic_intrp_image.jpg", quadratic_intrp_image)
         '''
 
-        cv2.imwrite("/home/sun/Desktop/Vanishing-Point-Detection/interpolation_image/quadratic_intrp_image_"+str(idx_) + "_" + str(seq_len_) +".jpg", annot_image)
-        #cv2.waitKey(-1)
+        cv2.imwrite("/home/sun/Desktop/Vanishing-Point-Detection/interpolation_image/vanishing_points_"+str(idx_) + "_" + str(seq_len_) +".jpg", annot_image)
+        # cv2.waitKey(-1)
 
 
 if __name__ == '__main__':
